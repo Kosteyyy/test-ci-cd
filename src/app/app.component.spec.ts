c7795dc2cfe6f1615629387cf1069e1280b30a93
Kosteyyy <averkiev@yandex.ru>
2024-04-21 23:27:05 +0300

    Сломал тест

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,7 @@ describe('AppComponent', () => {
   it(`should have as title 'test-cicd'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-cicd');
+    expect(app.title).toEqual('test-ci-cd');
   });
 
   it('should render title', () => {
